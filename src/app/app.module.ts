5a1c88859aa29811cb0cfcc39f829cefcd34cced
Mushegh Zakaryan <Mushegh@onsweb.nl>
2022-10-12 16:42:04 +0400

    clean

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {NgNumbersOnlyInputDirectiveModule} from "ng-numbers-only-input-directive";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgNumbersOnlyInputDirectiveModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
